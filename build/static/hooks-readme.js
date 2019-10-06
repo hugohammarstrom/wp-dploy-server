4ed0f01c1fa0198bfcccce8185c33b166d387bbf
hugohammarstrom <hugo.hammarstrom@icloud.com>
2019-10-06 13:15:17 +0200

    fix gitignore

--- build/static/hooks-readme.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _default = "Avaliable hooks:\n\npre-deploy\npre-download\npost-deploy\npost-download\n";
-exports["default"] = _default;
