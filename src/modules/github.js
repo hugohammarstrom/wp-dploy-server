3172ae294df01d428200479aaf632659bbd077d2
hugohammarstrom <hugo.hammarstrom@icloud.com>
2019-10-08 21:03:14 +0200

    use hash to decide if commit needs to be downloaded

--- src/modules/github.js
+++ src/modules/github.js
@@ -4,8 +4,10 @@ import isRelativeUrl from 'is-relative-url';
 import fs from "fs-extra"
 import logger from "@modules/logger"
 import * as hooks from "@modules/hooks"
+import * as _state from "@modules/state"
 
 import simpleGit from "simple-git/promise"
+import { hashElement } from 'folder-hash';
 
 
 /**
@@ -46,19 +48,40 @@ export const request = function(path, opts={}){
 }
 
 export const download_commit = async function({commit, repo, dir}){
+  let state, hashedFolder;
   if(!config.github) return logger.error("No github config specified")
   let {username, token} = config.github
   if (commit){
     await fs.ensureDir(`${dir}/versions/`)
     if (fs.existsSync(`${dir}/versions/${commit.sha}`)) {
-      logger.log("Commit already downloaded - skipping...")
-      return
+      state = await _state.get({dir})
+      hashedFolder = await hashElement(`${dir}/versions/${commit.sha}`)
+      if (state.versions && state.versions[commit.sha] && state.versions[commit.sha].hash === hashedFolder.hash){
+        logger.log("Commit already downloaded - skipping...")
+        return
+      } else {
+        await fs.remove(`${dir}/versions/${commit.sha}`)
+        logger.log("Commit changed - downloading again...")
+      }
+
     } else {
       logger.log("Downloading commit")
       await hooks.run({name: "pre-download", dir})
     }
     await simpleGit(`${dir}/versions`).clone(`https://${username}:${token}@github.com/${repo}`, commit.sha, ["--depth", "1"])
     await fs.remove(`${dir}/versions/${commit.sha}/.git`)
+    
+    
+    state = await _state.get({dir})
+    hashedFolder = await hashElement(`${dir}/versions/${commit.sha}`)
+    state.versions = state.versions || {}
+    state.versions[commit.sha] = {...state.versions[commit.sha], ...{
+      hash: hashedFolder.hash,
+      downloaded: Date.now()
+    }}
+
+    await _state.set({dir, state})
+
     await hooks.run({name: "post-download", dir})
   }
 }
--- src/modules/state.js
+++ src/modules/state.js
@@ -2,21 +2,34 @@ import fs from "fs-extra"
 import config from "@modules/config"
 import logger from "@modules/logger"
 
-export const get = async function({site: site_name}){
-  let site = config.sites.find(site => site.name == site_name);
-  if (!site){
-    logger.error("Site not found")
-    process.exit(-1)
+export const get = async function({site: site_name, dir}){
+  if (!dir){
+    let site = config.sites.find(site => site.name == site_name);
+    if (!site){
+      logger.error("Site not found")
+      process.exit(-1)
+    }
+    dir = site.dir
   }
 
-  if (!fs.existsSync(`${site.dir}/state.json`)){
-    await fs.writeJSON(`${site.dir}/state.json`, {history: []})
+  if (!fs.existsSync(`${dir}/state.json`)){
+    await fs.writeJSON(`${dir}/state.json`, {history: []})
   }
 
-  return await fs.readJson(`${site.dir}/state.json`)
+  return await fs.readJson(`${dir}/state.json`)
 }
 
-export const set = async function({site: site_name, state}){
-  let site = config.sites.find(site => site.name == site_name);
-  await fs.writeJSON(`${site.dir}/state.json`, state)
+export const set = async function({site: site_name, state, dir}){
+  if (!dir){
+    let site = config.sites.find(site => site.name == site_name);
+    if (!site){
+      logger.error("Site not found")
+      process.exit(-1)
+    }
+    dir = site.dir
+  }
+  
+  await fs.writeJSON(`${dir}/state.json`, state, {
+    spaces: 2
+  })
 }
